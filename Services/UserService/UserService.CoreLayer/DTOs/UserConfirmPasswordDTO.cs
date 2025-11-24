60e4e3a86da7ba91a1813a2052fa31209b60019d
Владислав <vladuk2332@gmail.com>
2025-11-24 20:56:30 +0300

    finished user CoreLayer

---
+++ Services/UserService/UserService.CoreLayer/DTOs/UserConfirmPasswordDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.CoreLayer.DTOs
+{
+    public class UserConfirmPasswordDTO
+    {
+        [Required(ErrorMessage = "Не указан адрес")]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Services/UserService/UserService.CoreLayer/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.CoreLayer.Entities;
+
+namespace UserService.CoreLayer
+{
+    public interface IUserRepository
+    {
+        public Task<IEnumerable<User>> GetAllAsync();
+        public Task<User?> GetAsync(int id);
+        public Task AddAsync(User user);
+        public Task UpdateAsync(int id, User newUser);
+        public Task DeleteAsync(int id);
+    }
+}
