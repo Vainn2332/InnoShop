b9d3d5876f205c73dac4c044bc4bc7ca02a096c5
Владислав <vladuk2332@gmail.com>
2025-11-29 23:50:42 +0300

    added post unitTest in ProductMicroService

---
+++ Services/ProductService/ProductService.Tests/ProductsControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ProductService.API.Controllers;
+using ProductService.ApplicationLayer.Interfaces;
+using ProductService.CoreLayer.DTOs;
+using ProductService.CoreLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProductService.Tests
+{
+    public class ProductsControllerTest
+    {
+        [Fact]
+        public async Task PostProductReturnsSuccess()
+        {
+            var jwtInfo = new JWTInfo()
+            {
+                EmailAddress = "abcd@example.com",
+                UserId = 1
+            };
+            ProductPostAndPutDTO productDTO = new ProductPostAndPutDTO()
+            {
+                Name="testName",
+                Description="testDescription",
+                Price=1                
+            };
+           
+            var productServiceMock = new Mock<IProductsService>();
+            var authServiceMock = new Mock<IAuthService>();
+
+            //var httpMock = new Mock<HttpRequest>();
+            var httpContext = new DefaultHttpContext().Request;
+
+            authServiceMock.Setup(p => p.GetJWTFromHeader(It.IsAny<HttpRequest>())).Returns("jwt");
+            authServiceMock.Setup(p => p.ParseJWT("jwt")).Returns(jwtInfo);
+            var userServiceMock = new Mock<IUserService>();
+            var controller = new ProductsController(productServiceMock.Object,
+                authServiceMock.Object, userServiceMock.Object);
+
+            var result = await controller.Post(productDTO);
+
+            productServiceMock.Verify(p => p.AddProductAsync(It.IsAny<Product>()  
+            ));
+            Assert.IsType<OkResult>(result);
+
+        }
+    }
+}
