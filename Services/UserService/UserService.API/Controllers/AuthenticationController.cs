ed73d707bb84cc60fa8120cf6f1a4eb998007746
vladuk2332 <vladuk2332@gmail.com>
2025-12-22 16:33:50 +0300

    backup

--- Services/UserService/UserService.API/Controllers/AuthenticationController.cs
+++ Services/UserService/UserService.API/Controllers/AuthenticationController.cs
@@ -6,7 +6,7 @@ using UserService.CoreLayer.DTOs;
 using UserService.CoreLayer.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//
 namespace UserService.API.Controllers
 {
     [Route("api/authentication")]
